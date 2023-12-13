ec4eed356b8a766949ef5b1d81f9a583a9b2ea0e
AnyCoba <105673865+AnyCoba@users.noreply.github.com>
2023-12-14 00:12:27 +0100

    Add files via upload

--- app_model.py
+++ app_model.py
@@ -2,50 +2,101 @@ from flask import Flask, request, jsonify
 import os
 import pickle
 import sqlite3
+import pandas as pd
 
-
-os.chdir(os.path.dirname(__file__))
+dir_path = os.path.dirname(os.path.abspath(__file__))
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+path_sql = os.path.join(dir_path,"data","api_database.db")
+path_model = os.path.join(dir_path,"data","advertising_model")
+path_csv = os.path.join(dir_path, "data", "Advertising.csv")
+
+df = pd.read_csv(path_csv, index_col=0)
+df["newpaper"] = df["newpaper"].apply(lambda x: x.replace("s",""))
+df.newpaper = df["newpaper"].astype("float")
+
+conn = sqlite3.connect(path_sql)
+df.to_sql('datos', conn, index=False, if_exists='replace')
+conn.close()
+
 # Función para conectar a la base de datos y obtener los datos necesarios
 def get_data_from_database():
-
-    conn = sqlite3.connect(r"C:/Users/Ana/Desktop/4. BOOTCAMP/Ana Fdz/4-Data_Engineering/1-APIs/Model/ejercicio/api_database.db")
-    query = conn.execute("""
-            SELECT tv, radio, newpaper from datos LIMIT 1
-             """)
-
-    data = query.fetchone()
+    conn = sqlite3.connect(path_sql)
+    query = conn.execute('''SELECT tv, radio, newpaper, sales from datos''')
+    data = query.fetchall()
     conn.close()
     return data
 
+def ingest_data(tv, radio, newpaper, sales):
+    conn = sqlite3.connect(path_sql)
+    conn.execute("""
+                        INSERT INTO datos (tv, radio, newpaper, sales)
+                        VALUES (?,?,?,?)
+                         """, (tv, radio, newpaper, sales))
+    conn.commit()
+    # Cerrar la conexión
+    conn.close()
+
 @app.route("/", methods=['GET'])
 def hello():
-    return "Bienvenido a mi API del modelo advertising"
+    return f"Bienvenido a mi API del modelo advertising" 
 
 # 1. Endpoint que devuelva la predicción de los nuevos datos enviados mediante argumentos en la llamada
-@app.route('/v1/predict', methods=['GET'])
+@app.route('/predict', methods=['GET'])
 def predict():
-    model = pickle.load(open('data/advertising_model','rb'))
-
-    # Obtener datos de la base de datos
-    database_data = get_data_from_database()
-
-    # Verificar si se obtuvieron datos de la base de datos
-    if database_data is None:
-        return "No se pudieron obtener datos de la base de datos"
-
-    # Desempacar los datos
-
-    tv, radio, newspaper = database_data
+    data = request.json
+    if data is None or 'data' not in data:
+        return jsonify({'error': 'Datos no proporcionados o formato incorrecto'}), 400
+    
+    df = pd.DataFrame(data['data'])
+    df.columns = ['TV', 'radio', 'newspaper']
+    model = pickle.load(open(path_model, 'rb'))
+    prediction = model.predict(df)
+    return jsonify({'prediction': str(round(prediction[0], 2)) + 'k €'})
 
 
-    if tv is None or radio is None or newspaper is None:
-        return "Missing args, the input values are needed to predict"
-    else:
-        prediction = model.predict([[int(tv),int(radio),int(newspaper)]])
-        return "The prediction of sales investing that amount of money in TV, radio and newspaper is: " + str(round(prediction[0],2)) + 'k €'
+# Función para almacenar nuevos datos
+@app.route('/ingest', methods=['POST'])
+def ingest_data_endpoint():
+    data = request.json
+    if data is None or 'data' not in data:
+        return jsonify({'error': 'Datos no proporcionados o formato incorrecto'}), 400
 
-app.run()
+    values = data['data']
+    for value_list in data['data']:
+        if not isinstance(value_list, list) or len(value_list) != 4:
+            return jsonify({'error': 'Formato de datos incorrecto'}), 400
+        ingest_data(*value_list)
+    return jsonify({'message': 'Datos ingresados correctamente'})
+
+
+
+
+@app.route('/retrain', methods=['POST'])
+def retrain_data():
+    
+    datos = get_data_from_database()
+
+    df = pd.DataFrame(datos)
+
+    df.columns = ['TV', 'radio', 'newpaper', 'sales']
+
+    X = df[['TV', 'radio', 'newpaper']]
+    y = df[['sales']]
+
+    # Cargar modelo existente
+    with open(path_model, 'rb') as f:
+        old_model = pickle.load(f)
+
+    # Reentrenar el modelo
+    new_model = old_model.fit(X, y)
+
+
+    with open(os.path.join(dir_path,"data", "new_model"), 'wb') as f:
+        pickle.dump(new_model, f)
+
+    return jsonify({'message': 'Modelo reentrenado correctamente.'})
+
+app.run(host="0.0.0.0", port=5000)
